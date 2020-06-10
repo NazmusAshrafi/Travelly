5a4744d7d62f591bd007caf28469f232f9e21b3d
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2020-06-10 14:37:07 +0600

    deploy

--- client/src/store.js
+++ client/src/store.js
@@ -5,32 +5,32 @@ import rootReducer from "./reducers"; //index.js
 const initialState = {};
 const middleware = [thunk];
 
-if (process.env.NODE_ENV === "production") {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(applyMiddleware(...middleware))
-  );
-} else {
-  store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-        window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
-}
+// if (process.env.NODE_ENV === "production") {
+//   store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(applyMiddleware(...middleware))
+//   );
+// } else {
+//   store = createStore(
+//     rootReducer,
+//     initialState,
+//     compose(
+//       applyMiddleware(...middleware),
+//       window.__REDUX_DEVTOOLS_EXTENSION__ &&
+//         window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+//   );
+// }
 
-// const store = createStore(
-//   rootReducer,
-//   initialState,
-//   compose(
-//     applyMiddleware(...middleware),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-//root reducer,initial state,middleware
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+root reducer,initial state,middleware
 
 export default store;
