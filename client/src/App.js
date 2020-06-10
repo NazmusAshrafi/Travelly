ceef8c6126a667a1003ea820057f8e1bfb4c6d28
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2020-06-10 16:28:40 +0600

    deploy

--- client/src/App.js
+++ client/src/App.js
@@ -45,7 +45,7 @@ const theme = createMuiTheme({
   },
 });
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "http://192.168.0.100/";
 
 // Check for token
 if (localStorage.jwtToken) {
--- server.js
+++ server.js
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000 || "http://192.168.0.100/";
 
 app.listen(port, () => {
   console.log(`port running at ${port}...`);
